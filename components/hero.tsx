ead8769a2d14f8a3dd0f15a04b93b60f7f2a1a18
Nathan A <nathanan@nathans-air.mynetworksettings.com>
2024-05-02 00:01:24 -0400

    Added forms

--- components/hero.tsx
+++ components/hero.tsx
@@ -28,10 +28,10 @@ export default function Hero() {
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Supercharge your development in an editor designed for less coding, with AI.</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
-                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Coming Soon</a>
+                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://forms.gle/171UyimgQJhEJbhU7">Join Waitlist</a>
               </div>
               <div data-aos="fade-up" data-aos-delay="600">
-                <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Learn more</a>
+                <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="https://forms.gle/5APwrsWLZ8VTvuKE7">Apply For Moderation Role</a>
               </div>
             </div>
           </div>
