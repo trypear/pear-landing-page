6dd179fc86912848cc0854144913ed60b0921984
Nathan A <nathanan@nathans-air.mynetworksettings.com>
2024-05-17 22:10:53 -0400

    Added about

--- components/about.tsx
+++ components/about.tsx
@@ -31,7 +31,7 @@ export default function AboutComponent() {
                 <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="https//youtube.com/nang88">Watch Nang</a>
               </div>
               <div data-aos="fade-up" data-aos-delay="400">
-                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="https//youtube.com/@FryingPan">Watch Pan</a>
+                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full sm:w-auto sm:ml-4" href="https//youtube.com/@FryingPan">Watch Pan</a>
               </div>
             </div>
           </div>
