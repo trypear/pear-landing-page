f4ca418ea10b24ddb3c8d3f836ab807ce196bb60
Shriprasanna <shriprasanna32@gmail.com>
2024-05-30 20:24:24 +0530

    update : about page canonical url

--- app/(about)/about/page.tsx
+++ app/(about)/about/page.tsx
@@ -1,11 +1,11 @@
-import AboutComponent from '@/components/about'
-import { constructMetadata } from '@/lib/utlis'
-import { Metadata } from 'next/types'
+import AboutComponent from "@/components/about"
+import { constructMetadata } from "@/lib/utlis"
+import { Metadata } from "next/types"
 
-export const metadata:Metadata = constructMetadata({
-  title: 'About',
-  description: 'About the team behind pear.ai',
-  canonical: "/"
+export const metadata: Metadata = constructMetadata({
+  title: "About",
+  description: "About the team behind pear.ai",
+  canonical: "/about",
 })
 
 export default function About() {
