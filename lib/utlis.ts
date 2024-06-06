dfed85a9b53dfab1283e2c4b0743afed06821bb9
Shriprasanna <shriprasanna32@gmail.com>
2024-06-06 22:41:33 +0530

    Init shadcn UI

--- lib/utlis.ts
+++ lib/utlis.ts
@@ -1,3 +1,10 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
 type MetadataProps = {
   title?: string
   description?: string
