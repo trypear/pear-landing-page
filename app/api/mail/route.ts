7f82e18f500223c22865459a57d6e31fa8f5c14c
Shriprasanna <shriprasanna32@gmail.com>
2024-06-08 01:41:00 +0530

    Add email sending functionality and selecting template functionality

--- app/api/mail/route.ts
+++ app/api/mail/route.ts
@@ -1,21 +1,28 @@
-import { NextRequest, NextResponse } from "next/server"
 import { Resend } from "resend"
+import { SelectEmailTemplate } from "@/components/email-templates"
+import { NextRequest, NextResponse } from "next/server"
 
 const resend = new Resend(process.env.RESEND_API_KEY!)
 
 export async function POST(req: NextRequest) {
-  const { body } = await req.json()
+  const body = await req.json()
+  const { to, subject, data } = body
+  const EmailTemplate = SelectEmailTemplate(data)
 
   try {
     const { data, error } = await resend.emails.send({
-      from: "onboarding@resend.dev",
-      to: "shriprasanna32@gmail.com",
-      subject: "Hello world",
-      react: "sasa",
+      from: "Acme <onboarding@resend.dev>", // Pear AI <accounts@pear.ai>
+      to: [to],
+      subject,
+      react: EmailTemplate as JSX.Element,
     })
 
+    if (error) {
+      return NextResponse.json({ error, status: 500 })
+    }
+
     return NextResponse.json({ message: "Email sent!", data, status: 200 })
-  } catch (err) {
-    return NextResponse.json({ error: err, status: 400 })
+  } catch (error) {
+    return NextResponse.json({ error, status: 500 })
   }
 }
