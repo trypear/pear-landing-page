753b114c79301377d8d87a8fefbc1b431fc343bd
Ellahi <ellahinator02@gmail.com>
2024-06-09 17:30:38 -0400

    settings - use constructMetadata fn

--- app/(profile)/settings/layout.tsx
+++ app/(profile)/settings/layout.tsx
@@ -1,7 +1,11 @@
 import Page from "./page";
-export const metadata = {
+import { constructMetadata } from "@/lib/utils"
+import { Metadata } from "next/types"
+
+export const metadata: Metadata = constructMetadata({
   title: 'Settings',
   description: 'Settings for your account.',
-}
+  canonical: '/settings',
+})
 
 export default Page;
