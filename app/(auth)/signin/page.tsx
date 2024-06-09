1bb06637a22b0f3a741441da3e4ae4a07f231874
Ellahi <ellahinator02@gmail.com>
2024-06-09 16:24:12 -0400

    Redirect to home page in production for auth pages

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -6,6 +6,10 @@ export default async function SignIn() {
   const supabase = createClient()
   const { data } = await supabase.auth.getUser()
 
+  if (process.env.NODE_ENV === 'production') { // Temporarily redirect to home page in production until we have a proper backend auth flow
+    redirect('/')
+  }
+
   if (data?.user) {
     redirect('/')
   }
