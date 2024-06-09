c6035a997080b067bc71cccfcda7dc341d4b4479
Duke Pan <59063950+Fryingpannn@users.noreply.github.com>
2024-06-09 18:24:08 -0400

    Revert "Supabase Auth #12"

--- app/(auth)/reset-password/layout.tsx
+++
@@ -1,7 +0,0 @@
-import Page from "./page";
-export const metadata = {
-  title: 'Reset Password - Open PRO',
-  description: 'Page description',
-}
-
-export default Page;
--- app/(auth)/signin/layout.tsx
+++
@@ -1,7 +0,0 @@
-import Page from "./page";
-export const metadata = {
-  title: 'Sign In - Open PRO',
-  description: 'Page description',
-}
-
-export default Page;
