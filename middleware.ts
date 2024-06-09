2b91c3438153f4537a4958451aa01c68031ec024
Duke Pan <matthewdukepan@gmail.com>
2024-06-09 19:09:54 -0400

    Revert "Revert "Add proper middleware flow + add temp redirect to signin and signup""
    This reverts commit 10c60c1b5bc8e206a5a79e2b492d37d01df33a54.

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { NextResponse, type NextRequest } from "next/server"
+import { updateSession } from "./utils/supabase/middleware"
+
+export async function middleware(request: NextRequest) {
+  if (
+    request.nextUrl.pathname.startsWith("/signin") ||
+    request.nextUrl.pathname.startsWith("/signup")
+  ) {
+    return NextResponse.redirect(new URL("/", request.url))
+  }
+  // temp redirect to home page in production until we have a proper backend auth flow
+  return await updateSession(request)
+}
+
+export const config = {
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/signin",
+    "/signup",
+  ],
+}
