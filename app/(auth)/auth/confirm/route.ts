0ae689a01bf88e11d2c38fe5e471f68952d09086
Ellahi <87584757+Ellahinator@users.noreply.github.com>
2024-06-15 12:11:47 -0400

    update signin error msg
    Co-authored-by: Duke Pan <59063950+Fryingpannn@users.noreply.github.com>

--- app/(auth)/auth/confirm/route.ts
+++ app/(auth)/auth/confirm/route.ts
@@ -28,6 +28,6 @@ export async function GET(request: NextRequest) {
   }
 
   // return the user to an error page with some instructions
-  redirectTo.pathname = '/signin?message=Could not verify OTP'
+  redirectTo.pathname = '/signin?message=cannot-verify-otp'
   return NextResponse.redirect(redirectTo)
 }
