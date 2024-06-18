a1cc90b88a1fd0c35d1d4d4641a5e39b2f82187b
jspEclipse <96797930+jspEclipse@users.noreply.github.com>
2024-06-18 15:34:45 +0000

    features commit

--- components/features.tsx
+++ components/features.tsx
@@ -1,322 +1,49 @@
+import Image from "next/image"
 export default function Features() {
   return (
-    <section>
-      <div className="mx-auto max-w-6xl px-4 sm:px-6">
-        <div className="py-12 md:py-20">
-          {/* Section header */}
-          <div
-            className="mx-auto max-w-3xl pb-12 text-center md:pb-20"
-            data-aos="fade-up"
-            data-aos-delay="1000"
-          >
-            <h2 className="h2 mb-4">Oh - and it&apos;s all Open Sourced.</h2>
-            <p className="text-xl text-gray-600">
-              Pear is made by developers, for developers. Help us out by giving
-              <a
-                href="https://github.com/trypear/pearai-app"
-                className="text-primary-500 hover:text-primary-700"
-              >
-                {" "}
-                the repo{" "}
-              </a>
-              a star! If you have questions, you can join our{" "}
-              <a
-                href="https://discord.gg/7QMraJUsQt"
-                className="text-primary-500 hover:text-primary-700"
-              >
-                Discord
-              </a>{" "}
-              and ask us directly.
-            </p>
-            <div
-              className="mt-4 flex justify-center space-x-4"
-              data-aos="fade-up"
-              data-aos-delay="600"
-            >
-              <a
-                className="btn w-full bg-primary-500 text-white-main hover:bg-primary-600 sm:w-auto"
-                href="https://forms.gle/5APwrsWLZ8VTvuKE7"
-              >
-                Apply For Moderation Role
-              </a>
-              <a
-                className="btn w-full bg-secondary-main text-white-main hover:bg-gray-800 sm:w-auto"
-                href="https://docs.google.com/presentation/d/1zR9-7DTlb2PcsnapryZw8jHSkLTs9JxeXth4nyeemAQ/edit?usp=sharing"
-              >
-                Contributing 101
-              </a>
-            </div>
-          </div>
-
-          {/* Items */}
-          <div
-            className="mx-auto grid max-w-sm items-start gap-8 md:max-w-2xl md:grid-cols-2 lg:max-w-none lg:grid-cols-3 lg:gap-16"
-            data-aos-id-blocks
-          >
-            {/* 1st item */}
-            <div
-              className="relative flex flex-col items-center"
-              data-aos="fade-up"
-              data-aos-anchor="[data-aos-id-blocks]"
-            >
-              <svg
-                className="mb-4 h-16 w-16"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  className="fill-current text-primary-500"
-                  width="64"
-                  height="64"
-                  rx="32"
-                />
-                <path
-                  className="stroke-current text-white-main"
-                  d="M30 39.313l-4.18 2.197L27 34.628l-5-4.874 6.91-1.004L32 22.49l3.09 6.26L42 29.754l-3 2.924"
-                  strokeLinecap="square"
-                  strokeWidth="2"
-                  fill="none"
-                  fillRule="evenodd"
-                />
-                <path
-                  className="stroke-current text-white-main"
-                  d="M43 42h-9M43 37h-9"
-                  strokeLinecap="square"
-                  strokeWidth="2"
-                />
-              </svg>
-              <h4 className="h4 mb-2">Knows your code.</h4>
-              <p className="text-center text-lg text-gray-600">
-                Pear has context on your codebase so you can ask questions about
-                your code.
-              </p>
-            </div>
-
-            {/* 2nd item */}
-            <div
-              className="relative flex flex-col items-center"
-              data-aos="fade-up"
-              data-aos-delay="100"
-              data-aos-anchor="[data-aos-id-blocks]"
-            >
-              <svg
-                className="mb-4 h-16 w-16"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <circle
-                  className="fill-current text-primary-500"
-                  cx="32"
-                  cy="32"
-                  r="32"
-                />
-                <path
-                  className="stroke-current text-white-main"
-                  strokeWidth="2"
-                  strokeLinecap="square"
-                  d="M21 23h22v18H21z"
-                  fill="none"
-                  fillRule="evenodd"
-                />
-                <path
-                  className="stroke-current text-white-main"
-                  d="M26 28h12M26 32h12M26 36h5"
-                  strokeWidth="2"
-                  strokeLinecap="square"
-                />
-              </svg>
-              <h4 className="h4 mb-2">Auto-Implement Features</h4>
-              <p className="text-center text-lg text-gray-600">
-                Ask for a feature and receive a PR implementing it.
-              </p>
-            </div>
-
-            {/* 3rd item */}
-            <div
-              className="relative flex flex-col items-center"
-              data-aos="fade-up"
-              data-aos-delay="200"
-              data-aos-anchor="[data-aos-id-blocks]"
-            >
-              <svg
-                className="mb-4 h-16 w-16"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  className="fill-current text-primary-500"
-                  width="64"
-                  height="64"
-                  rx="32"
-                />
-                <g
-                  transform="translate(21 21)"
-                  strokeLinecap="square"
-                  strokeWidth="2"
-                  fill="none"
-                  fillRule="evenodd"
-                >
-                  <ellipse
-                    className="stroke-current text-white-main"
-                    cx="11"
-                    cy="11"
-                    rx="5.5"
-                    ry="11"
-                  />
-                  <path
-                    className="stroke-current text-white-main"
-                    d="M11 0v22M0 11h22"
-                  />
-                  <circle
-                    className="stroke-current text-white-main"
-                    cx="11"
-                    cy="11"
-                    r="11"
-                  />
-                </g>
-              </svg>
-              <h4 className="h4 mb-2">UI/UX Focused</h4>
-              <p className="text-center text-lg text-gray-600">
-                Pear puts the user experience first, making developing as
-                seamless as possible.
-              </p>
-            </div>
-
-            {/* 4th item */}
-            <div
-              className="relative flex flex-col items-center"
-              data-aos="fade-up"
-              data-aos-delay="300"
-              data-aos-anchor="[data-aos-id-blocks]"
-            >
-              <svg
-                className="mb-4 h-16 w-16"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  className="fill-current text-primary-500"
-                  width="64"
-                  height="64"
-                  rx="32"
-                />
-                <g
-                  transform="translate(22 21)"
-                  strokeLinecap="square"
-                  strokeWidth="2"
-                  fill="none"
-                  fillRule="evenodd"
-                >
-                  <path
-                    className="stroke-current text-white-main"
-                    d="M17 22v-6.3a8.97 8.97 0 003-6.569A9.1 9.1 0 0011.262 0 9 9 0 002 9v1l-2 5 2 1v4a2 2 0 002 2h4a5 5 0 005-5v-5"
-                  />
-                  <circle
-                    className="stroke-current text-white-main"
-                    cx="13"
-                    cy="9"
-                    r="3"
-                  />
-                </g>
-              </svg>
-              <h4 className="h4 mb-2 text-center">Never Start From Scratch</h4>
-              <p className="text-center text-lg text-gray-600">
-                Pear comes with high-quality templates and boilerplate code for
-                any type of project.
-              </p>
-            </div>
-
-            {/* 5th item */}
-            <div
-              className="relative flex flex-col items-center"
-              data-aos="fade-up"
-              data-aos-delay="400"
-              data-aos-anchor="[data-aos-id-blocks]"
-            >
-              <svg
-                className="mb-4 h-16 w-16"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  className="fill-current text-primary-500"
-                  width="64"
-                  height="64"
-                  rx="32"
-                />
-                <g
-                  strokeLinecap="square"
-                  strokeWidth="2"
-                  fill="none"
-                  fillRule="evenodd"
-                >
-                  <path
-                    className="stroke-current text-white-main"
-                    d="M29 42h10.229a2 2 0 001.912-1.412l2.769-9A2 2 0 0042 29h-7v-4c0-2.373-1.251-3.494-2.764-3.86a1.006 1.006 0 00-1.236.979V26l-5 6"
-                  />
-                  <path
-                    className="stroke-current text-white-main"
-                    d="M22 30h4v12h-4z"
-                  />
-                </g>
-              </svg>
-              <h4 className="h4 mb-2">Batteries Included</h4>
-              <p className="text-center text-lg text-gray-600">
-                Pear comes fully setup with shortcuts and terminal plugins used
-                to supercharge development productivity.
-              </p>
-            </div>
-
-            {/* 6th item */}
-            <div
-              className="relative flex flex-col items-center"
-              data-aos="fade-up"
-              data-aos-delay="500"
-              data-aos-anchor="[data-aos-id-blocks]"
-            >
-              <svg
-                className="mb-4 h-16 w-16"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  className="fill-current text-primary-500"
-                  width="64"
-                  height="64"
-                  rx="32"
-                />
-                <g
-                  transform="translate(21 22)"
-                  strokeLinecap="square"
-                  strokeWidth="2"
-                  fill="none"
-                  fillRule="evenodd"
-                >
-                  <path
-                    className="stroke-current text-white-main"
-                    d="M17 2V0M19.121 2.879l1.415-1.415M20 5h2M19.121 7.121l1.415 1.415M17 8v2M14.879 7.121l-1.415 1.415M14 5h-2M14.879 2.879l-1.415-1.415"
-                  />
-                  <circle
-                    className="stroke-current text-white-main"
-                    cx="17"
-                    cy="5"
-                    r="3"
-                  />
-                  <path
-                    className="stroke-current text-white-main"
-                    d="M8.86 1.18C3.8 1.988 0 5.6 0 10c0 5 4.9 9 11 9a10.55 10.55 0 003.1-.4L20 21l-.6-5.2a9.125 9.125 0 001.991-2.948"
-                  />
-                </g>
-              </svg>
-              <h4 className="h4 mb-2">Familiar Feel</h4>
-              <p className="text-center text-lg text-gray-600">
-                Pear is a fork of VSCode, so you can pick up exactly where you
-                left off.
-              </p>
-            </div>
-          </div>
+    <section className="flex flex-col">
+      <div className="flex m-24 justify-between max-w-full" style={{height:"36.426rem"}}>
+        <div className="flex flex-col justify-center content-evenly">
+          <h1 className="text-4xl" style={{color:"#4CC9A7", fontWeight:"500"}} data-aos="fade-up" data-aos-delay="600">No more switching <br/> between files</h1>
+          <br/>
+          <div className="text-base" data-aos="fade-up" data-aos-delay="600">To add missing context: directly reference code by including other files in the chat by adding '@filename'. <br/><br/> This also works for folders, docs, terminal content, codebase, and more 😈 </div>
         </div>
+        <Image src="https://s3-alpha-sig.figma.com/img/7f2c/7849/66cefeccfdadce253b814003bc6eb932?Expires=1719792000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=lm~eIoDLovJFFV4BgGBdGGR2q62yBvTV0Bj7locbGT1ai5TrxHEl35ulH6~lnAEZi5im81i8z37vujv1W45ZU8iRAZKDBtdM8OkgMmeA4XZNKUMsy0aozeOQ84FhiylATOBGMfGtwJUxFbomtA4FZV7jTCc1v2NdvmoxAfTWHrMD8G-CMcB0i3sW-VassT7b~U6XIch12NzYEEoZ5y2OQJQmR4l3o3XumaYeOm7KuXhvujflKr-1G4ZcZMyx6B9~QjI0C2OLiSl8LQQilwE60lc2T6KLkgPptl27Gp2WG2ArT-8hCmk4E-vrwXT7RA~1bjNQtQz3eTukoNPIMifdEA__"
+        width={873.48}
+        height={582.82}
+        alt=""
+        className="rounded-lg"
+        data-aos="fade-up" 
+        data-aos-delay="600"/>
+      </div>
+      <div className="flex m-24 justify-between max-w-full" style={{height:"36.426rem"}}>
+        <div className="flex flex-col justify-center content-evenly">
+          <h1 className="text-4xl" style={{color:"#4CC9A7", fontWeight:"500"}} data-aos="fade-up" data-aos-delay="600">No more tedious changes,<br/>or forgetting language<br/>syntax</h1>
+          <br/>
+          <div className="text-base" data-aos="fade-up" data-aos-delay="600">Directly make changes inline by pressing CMD+I (ALT+L on Windows), and choose what you want to keep. <br/><br/> Here, we ask Pear to help us handl edge cases 😏</div>
+        </div>
+        <Image src="https://s3-alpha-sig.figma.com/img/9229/8c19/f69e8c788124e01751223a500df85aea?Expires=1719792000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=nBte2jUC56Kr11M1WApQo-LQyYWAIYyxrmSVNyOgOZKMPAfN0Wem7dACcXkpI8T-ytGA~pA-CHwyZ9fxKAlD88kBI1ZAgFmDEDCsIhuBBOVYtc0XyPkC2S0R~yQAqgUUMUqG8Si34HvSDfq4N9x1vcZi3osCfpbPY7vFKJCze-E538Hw-7aA4wWAC1h-MLnPM0XvMZoFwPwBXB95ogiKaG8qGfFlSegAnupb2sqR2TmfLse6dG5jqk-dOpzUF3zbnH-9Q-nkJK9Fmdw~LHvj4z3wcpN75bWpNSgzyuXR3GXwJPxamvxs9Af9VNj8l2DCS~p4YLe-C1iouGrkV-h-yw__"
+        width={873.48}
+        height={582.82}
+        alt=""
+        className="rounded-lg"
+        data-aos="fade-up"
+        data-aos-delay="600"/>
+      </div>
+      <div className="flex m-24 justify-between max-w-full" style={{height:"36.426rem"}}>
+        <div className="flex flex-col justify-center content-evenly">
+          <h1 className="text-4xl" style={{color:"#4CC9A7", fontWeight:"500"}} data-aos="fade-up" data-aos-delay="600">No more tiresome <br/> copy pasting.</h1>
+          <br/>
+          <div className="text-base" data-aos="fade-up" data-aos-delay="600">Directly bring your code to the chat by selecting it and pressing CMD+L (ALT+L on Windows).<br/><br/> Prompt it right away 😎</div>
+        </div>
+        <Image src="https://s3-alpha-sig.figma.com/img/84e7/f5f7/d4fd2d4731a8b34bba6ea49aaa93fc2e?Expires=1719792000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gTnsx4tuu8Abf3fDOvjyhZmriONDkCKc0FjEzJn~dJC48cXCi2n6oqXzWOjGCr20CQRbTH6S9yIn7Afgm7Sq~zvSGXLR6h7ZmD-ogBAX5HkO9XBANOefSWiRBCn~IvC5JlKFwMBiPvtw8abggElNFwfS4LdvSxA~k-QgEWhgrxg5irFkcQKGR-dUrehhTU-WDoKusPIjmyD09V4UBWcJ83c9FyllMi03ul-bpxnQYK2L-KPRavLpdWERCNfOaqbH-TjvdlrqR62-hOJ2dQIPMGQTDWDuYa9xEzny0muRItEIjO-xOA-2~d458wBaDlP6zp8azsGchmtEoqpSvZSwig__"
+        width={873.48}
+        height={582.82}
+        alt=""
+        className="rounded-lg"
+        data-aos="fade-up" 
+        data-aos-delay="600"/>
       </div>
     </section>
-  );
+  )
 }
--- next.config.js
+++ next.config.js
@@ -1,17 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {}
 
 // next.config.js
 const nextConfiguration = {
-  ...nextConfig,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
-  },
-};
-
-module.exports = nextConfiguration;
+    ...nextConfig,
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["@svgr/webpack"]
+      });
+  
+      return config;
+    },
+    images: {
+      domains: ["s3-alpha-sig.figma.com"]
+    }
+  };
+  
+  module.exports = nextConfiguration;
+  
