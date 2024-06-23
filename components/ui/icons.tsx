cac9d899a3e5c9edc784a3804b2972459ad0c0b1
Duke Pan <59063950+Fryingpannn@users.noreply.github.com>
2024-06-23 17:48:15 -0400

    Update components/ui/icons.tsx

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -24,7 +24,7 @@ export const GoogleLogo: FC<ComponentProps<"svg">> = (props) => (
   </svg>
 );
 
-export const XMark: FC<ComponentProps<"svg">> = (props) => (
+export const XMarkIcon: FC<ComponentProps<"svg">> = (props) => (
   <svg viewBox="0 0 16 16" fill="currentColor" {...props}>
     <path d="M12.72 3.293a1 1 0 00-1.415 0L8.012 6.586 4.72 3.293a1 1 0 00-1.414 1.414L6.598 8l-3.293 3.293a1 1 0 101.414 1.414l3.293-3.293 3.293 3.293a1 1 0 001.414-1.414L9.426 8l3.293-3.293a1 1 0 000-1.414z" />
   </svg>
