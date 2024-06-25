93d08298c14b256810f41c34c0e12469e794dcab
iceprey <abuwswork@gmail.com>
2024-06-25 13:48:22 -0700

    refactor(auth): Implement try/catch & improve error handling

--- app/(auth)/actions.ts
+++ app/(auth)/actions.ts
@@ -2,11 +2,12 @@
 
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-
 import { createClient } from "@/utils/supabase/server";
 import {
+  AuthError,
   SignInWithPasswordCredentials,
   SignUpWithPasswordCredentials,
+  OAuthResponse,
 } from "@supabase/supabase-js";
 
 const supabase = createClient();
@@ -17,65 +18,59 @@ export async function signin(formData: FormData) {
   const email: string = formData.get("email")?.toString() ?? "";
   const password: string = formData.get("password")?.toString() ?? "";
   const redirectUrl: URL = new URL(formData.get("redirect")?.toString() ?? "/");
-  console.log(redirectUrl.host);
-  console.log(email);
-  console.log(password);
 
-  const { data, error } = await supabase.auth.signInWithPassword({
-    email,
-    password,
-  });
-
-  if (error) {
-    return { error: error.message };
+  try {
+    const { data } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    const token: string | undefined = data?.session?.access_token;
+    if (token && redirectUrl.hostname) {
+      redirectUrl.searchParams.append("token", token);
+    }
+    revalidatePath("/", "layout");
+    redirect(redirectUrl.toString());
+  } catch (error) {
+    if (error instanceof AuthError) {
+      return { error: error.message };
+    }
+    return { error: "An error occurred while signing in" };
   }
-
-  const token: string = data?.session?.access_token ?? undefined;
-  if (token && redirectUrl.hostname) {
-    redirectUrl.searchParams.append("token", token);
-  }
-
-  revalidatePath("/", "layout");
-  redirect(redirectUrl.toString());
 }
 
-// Google OAuth sign-in
 export async function signinWithGoogle(formData: FormData) {
   const redirectUrl: URL = new URL(formData.get("redirect")?.toString() ?? "/");
-  const { data, error } = await supabase.auth.signInWithOAuth({
-    provider: "google",
-    options: {
-      redirectTo: `${process.env.NEXT_PUBLIC_REDIRECT_URL}/auth/callback?next=${encodeURIComponent(redirectUrl.toString())}`,
-    },
-  });
-  if (error) {
+
+  try {
+    const { data }: OAuthResponse = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `${process.env.NEXT_PUBLIC_REDIRECT_URL}/auth/callback?next=${encodeURIComponent(redirectUrl.toString())}`,
+      },
+    });
+    if (data.url) {
+      redirect(data.url);
+    }
+    revalidatePath("/", "layout");
+  } catch (error) {
     redirect("/error");
   }
-
-  if (data.url) {
-    redirect(data.url);
-  }
-
-  revalidatePath("/", "layout");
 }
 
-// Reset password
 export async function resetPassword(formData: FormData) {
-  const { data, error } = await supabase.auth.resetPasswordForEmail(
-    formData.get("email") as string,
-  );
-
-  if (error) {
-    return { error: error.message };
+  try {
+    await supabase.auth.resetPasswordForEmail(formData.get("email") as string);
+    revalidatePath("/", "layout");
+    redirect("/");
+  } catch (error) {
+    if (error instanceof AuthError) {
+      return { error: error.message };
+    }
+    return { error: "An error occurred while resetting password" };
   }
-
-  revalidatePath("/", "layout");
-  redirect("/");
 }
 
 export async function signup(formData: FormData) {
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
   const data: SignUpWithPasswordCredentials = {
     email: formData.get("email") as string,
     password: formData.get("password") as string,
@@ -87,12 +82,14 @@ export async function signup(formData: FormData) {
     },
   };
 
-  const { error } = await supabase.auth.signUp(data);
-
-  if (error) {
-    return { error: error.message };
+  try {
+    await supabase.auth.signUp(data);
+    revalidatePath("/", "layout");
+    redirect("/");
+  } catch (error) {
+    if (error instanceof AuthError) {
+      return { error: error.message };
+    }
+    return { error: "An error occurred while signing up" };
   }
-
-  revalidatePath("/", "layout");
-  redirect("/");
 }
