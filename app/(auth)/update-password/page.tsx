6316d425673fd7d5961c1de9d5ae995c2689b04f
Ellahi <ellahinator02@gmail.com>
2024-06-29 11:11:09 -0400

    auth protect update password page

--- app/(auth)/update-password/page.tsx
+++ app/(auth)/update-password/page.tsx
@@ -1,6 +1,8 @@
 import UpdatePasswordForm from "@/components/auth/update-password-form";
 import { constructMetadata } from "@/lib/utils";
+import { createClient } from "@/utils/supabase/server";
 import { Metadata } from "next";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = constructMetadata({
   title: "Update password",
@@ -8,13 +10,19 @@ export const metadata: Metadata = constructMetadata({
   canonical: "/update-password",
 });
 
-const UpdatePasswordPage = async () => (
-  <div className="mx-auto max-w-md pt-44 md:pt-32">
-    <h1 className="text-center text-3xl font-semibold md:text-5xl md:font-normal">
-      Update Password
-    </h1>
-    <UpdatePasswordForm />
-  </div>
-);
+export default async function UpdatePasswordPage() {
+  const supabase = createClient();
 
-export default UpdatePasswordPage;
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    redirect("/signin");
+  }
+  return (
+    <div className="mx-auto max-w-md pt-44 md:pt-32">
+      <h1 className="text-center text-3xl font-semibold md:text-5xl md:font-normal">
+        Update Password
+      </h1>
+      <UpdatePasswordForm />
+    </div>
+  );
+}
