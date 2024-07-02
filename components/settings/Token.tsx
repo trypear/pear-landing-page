2620ed8ecd12542e12f41d82add317dbf558ce8b
akash3444 <akashmoradiya3444@gmail.com>
2024-07-02 21:43:35 +0530

    feat(settings): add clipboard copy functionality for access and refresh tokens

--- components/settings/Token.tsx
+++ components/settings/Token.tsx
@@ -1,4 +1,7 @@
-import { CopyIcon } from "lucide-react";
+"use client";
+
+import { useCopyToClipboard } from "@/hooks/useCopyToClipboard";
+import { CheckIcon, CopyIcon } from "lucide-react";
 import { FC } from "react";
 import { Button } from "../ui/button";
 
@@ -8,6 +11,8 @@ interface TokenProps {
 }
 
 const Token: FC<TokenProps> = ({ name, value }) => {
+  const { isCopied, copy } = useCopyToClipboard();
+
   return (
     <div className="text-sm">
       <p className="mb-0.5 font-medium">{name}:</p>
@@ -15,8 +20,8 @@ const Token: FC<TokenProps> = ({ name, value }) => {
         <div className="no-scrollbar h-7 w-full select-all overflow-x-auto whitespace-nowrap rounded-md border px-1.5 py-0.5">
           {value}
         </div>
-        <Button className="h-7 rounded-md px-1.5">
-          <CopyIcon size={16} />
+        <Button className="h-7 rounded-md px-1.5" onClick={() => copy(value)}>
+          {isCopied ? <CheckIcon size={16} /> : <CopyIcon size={16} />}
         </Button>
       </div>
     </div>
---
+++ hooks/useCopyToClipboard.ts
@@ -0,0 +1,38 @@
+import { useCallback, useState } from "react";
+
+type CopiedValue = string | null;
+
+type CopyFn = (text: string) => Promise<boolean>;
+
+export function useCopyToClipboard(): {
+  isCopied: boolean;
+  copiedText: CopiedValue;
+  copy: CopyFn;
+} {
+  const [copiedText, setCopiedText] = useState<CopiedValue>(null);
+  const [isCopied, setIsCopied] = useState(false);
+
+  const copy: CopyFn = useCallback(async (text) => {
+    if (!navigator?.clipboard) {
+      console.warn("Clipboard not supported");
+      return false;
+    }
+
+    // Try to save to clipboard then save it in the state if worked
+    try {
+      await navigator.clipboard.writeText(text);
+      setCopiedText(text);
+      setIsCopied(true);
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 3000);
+      return true;
+    } catch (error) {
+      console.warn("Copy failed", error);
+      setCopiedText(null);
+      return false;
+    }
+  }, []);
+
+  return { isCopied, copiedText, copy };
+}
