eb9e7bf4561d950e0dd1d5f99fada2ffca06dcbf
dscreet <discreetbinger@gmail.com>
2024-07-04 21:58:12 +0100

    search params now carry on to settings page if user is logged in

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,13 +1,36 @@
 import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
 import SignInComponent from "@/components/auth/signin";
+import { constructMetadata } from "@/lib/utils";
+import { Metadata } from "next/types";
 
-export default async function SignIn() {
+export const metadata: Metadata = constructMetadata({
+  title: "Sign In",
+  description: "Sign in to your account",
+  canonical: "/signin",
+});
+
+interface SignInProps {
+  searchParams: {
+    callback?: string;
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+export default async function SignIn({ searchParams }: SignInProps) {
   const supabase = createClient();
-  const { data } = await supabase.auth.getUser();
+  const { data: userData } = await supabase.auth.getUser();
+  const { data: sessionData } = await supabase.auth.getSession();
 
-  if (data?.user) {
-    redirect("/");
+  if (userData?.user) {
+    const callbackForDesktopApp = searchParams?.callback ?? "";
+    const accessToken = sessionData.session?.access_token ?? "";
+    const refreshToken = sessionData.session?.refresh_token ?? "";
+    const encodedAccessToken = encodeURIComponent(accessToken);
+    const encodedRefreshToken = encodeURIComponent(refreshToken);
+    return redirect(
+      `/settings?callback=${encodeURIComponent(callbackForDesktopApp)}&accessToken=${encodedAccessToken}&refreshToken=${encodedRefreshToken}`,
+    );
   }
   return (
     <>
