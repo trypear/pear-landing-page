683d7838ffcf84e82b7166ed7f38e357c7524417
Shriprasanna <shriprasanna32@gmail.com>
2024-07-05 01:35:30 +0530

    removed layout.tsx

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -1,6 +1,14 @@
 import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
 import ResetPasswordComponent from "@/components/auth/reset-password";
+import { constructMetadata } from "@/lib/utils";
+import { Metadata } from "next/types";
+
+export const metadata: Metadata = constructMetadata({
+  title: "Reset Password",
+  description: "Reset your password",
+  canonical: "/reset-password",
+});
 
 export default async function SignIn() {
   const supabase = createClient();
--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,6 +1,14 @@
 import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
 import SignInComponent from "@/components/auth/signin";
+import { constructMetadata } from "@/lib/utils";
+import { Metadata } from "next/types";
+
+export const metadata: Metadata = constructMetadata({
+  title: "Sign In",
+  description: "Sign in to your account",
+  canonical: "/signin",
+});
 
 export default async function SignIn() {
   const supabase = createClient();
