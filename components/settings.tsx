f420496a6b8abeec20e8486d46ad9105b033c535
Ellahi <ellahinator02@gmail.com>
2024-07-06 18:34:38 -0400

    prettier

--- components/settings.tsx
+++ components/settings.tsx
@@ -54,7 +54,7 @@ export default function SettingsPage({ initialSession }: SettingsPageProps) {
     }
 
     fetchUserAndHandleCallback();
-  }, [router, searchParams]);
+  }, [session, router, searchParams]);
 
   return (
     <section className="relative">
