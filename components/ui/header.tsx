5469b82be0052d7f65366ce1f42f5e3e772e9ffb
Tanishq Solanki <officialxinov@gmail.com>
2024-07-08 18:39:31 +0530

    modification in priority waitlist page ui / header centered / mobile menu

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -14,29 +14,33 @@ export default function Header() {
   return (
     <header className="fixed top-0 z-30 w-full animate-fadein-opacity bg-white-50 bg-opacity-80 shadow-sm backdrop-blur-[16px]">
       <div className="mx-auto max-w-screen-xl px-4 py-1 sm:px-6 sm:py-2">
-        <div className="text-md flex h-10 items-center justify-between text-secondary-600 transition ease-in-out sm:text-lg">
+        <div className="text-md flex w-full h-10 items-center justify-between text-secondary-600 transition ease-in-out sm:text-lg">
           {/* Site branding */}
-          <div className="flex flex-row items-start space-x-2">
+          <div className="flex flex-row items-center w-[14%] space-x-2">
             {/* Logo */}
             <Link className="-mt-0.5 sm:mt-0" href="/">
               <PearDarkLogo />
             </Link>
-            {/* Navigation */}
-            <nav>
-              <ul className="ml-4 flex justify-center space-x-6">
-                {navLinks.map((link) => (
-                  <li key={link.label}>
-                    <Link className="hover:text-secondary-400" href={link.path}>
-                      {link.label}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </nav>
           </div>
-          <AuthButton />
-          {/* AuthButton is hidden in production */}
-          <MobileMenu />
+
+          {/* Navigation */}
+          <nav>
+            <ul className="hidden md:flex w-full items-center justify-center space-x-6">
+              {navLinks.map((link) => (
+                <li key={link.label}>
+                  <Link className="hover:text-secondary-400" href={link.path}>
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+
+          <div className="flex flex-row justify-end items-center w-[14%]">
+            <AuthButton />
+            {/* AuthButton is hidden in production */}
+            <MobileMenu />
+          </div>
         </div>
       </div>
     </header>
