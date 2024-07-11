20179ecc83beb3f0477e8b8cbaa7c5e35da0189a
Duke Pan <matthewdukepan@gmail.com>
2024-07-12 00:40:22 +0800

    Send email for contact us page

--- components/pricing.tsx
+++ components/pricing.tsx
@@ -124,7 +124,7 @@ const PricingPage: React.FC<PricingPageProps> = ({ user }) => {
             <p className="text-base text-gray-400 sm:text-lg md:text-xl">
               Want to use Pear in your business?
               <Link
-                href="#"
+                href="mailto:trypearai@gmail.com"
                 className="ml-2 font-semibold text-primary-700 hover:text-primary-800"
                 aria-label="Contact us for custom plans"
               >
