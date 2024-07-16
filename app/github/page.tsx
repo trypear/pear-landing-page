404523e759f40725647c18d65ebf83506bee7b74
sehoonk2004 <107429645+sehoonk2004@users.noreply.github.com>
2024-07-16 16:10:42 -0400

    fixed GitHub capitalization errors

--- app/github/page.tsx
+++ app/github/page.tsx
@@ -3,15 +3,15 @@ import { constructMetadata } from "@/lib/utils";
 import { Metadata } from "next/types";
 
 export const metadata: Metadata = constructMetadata({
-    title: "GitHub",
-    description: "GitHub repositories PearAI",
-    canonical: "/github",
+  title: "GitHub",
+  description: "GitHub repositories PearAI",
+  canonical: "/github",
 });
 
 export default function github() {
-    return (
-        <>
-            <GithubPageComponent />
-        </>
-    );
+  return (
+    <>
+      <GithubPageComponent />
+    </>
+  );
 }
