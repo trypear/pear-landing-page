800764af7b4dae7413ad43b1d4506d4b94703d0d
himanshu <him.speedo@gmail.com>
2024-07-30 22:50:12 +0530

    more changes

--- app/changelog/page.tsx
+++ app/changelog/page.tsx
@@ -13,30 +13,40 @@ import TimelineItem from "./timeline";
 export default function ChangeLog() {
   const updates = [
     {
-      date: "31 September 2024",
-      title: "v.0.0.2 - Claude Sonnet Model, UI/UX improvements.",
-      description: "v.0.0.2 - Claude Sonnet Model, UI/UX improvements.",
-    },
-    {
-      date: "31 September 2024",
-      title: "Release v5.2.0 quick bug fix 🐞",
-      description: "",
-    },
-    {
-      date: "31 September 2024",
-      title: "Marked Install Chart completed",
-      description: "Finally! You can check it out here.",
-    },
-    {
-      date: "31 September 2024",
-      title: "Take a break ⛳️",
+      date: "6th September 2024",
+      title: "🌟 v.0.0.2 - Claude Sonnet Model, UI/UX improvements.",
       description: (
         <>
-          <ul className="list-disc">
-            <li>new updates</li>
-            <li>interesting updates</li>
-            <li>bug fixes</li>
+          <ul className="text-gray-60 list-disc space-y-1 ps-6 text-sm">
+            <li>New AI Model - Clude 3.5 Sonnet</li>
+            <li>GPT Autocompletions</li>
+            <li>UI/UX improvements</li>
+            <li>Bug Fixes like some extensions not working.</li>
           </ul>
+        </>
+      ),
+    },
+    {
+      date: "31 September 2024",
+      title: "Release v0.0.1 quick bug fix 🐞",
+      description: (
+        <>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam, nam
+            ipsum, ab similique ipsam natus voluptas asperiores voluptatem sint
+            odio id quia ad tempora ipsa quo voluptatibus, praesentium nostrum.
+            Vero, consequatur voluptates, porro itaque veniam aliquid
+            perferendis cupiditate suscipit illo totam velit quibusdam debitis
+            placeat optio officia recusandae. Dolore, odio.
+          </p>
+        </>
+      ),
+    },
+    {
+      date: "19 July 2024",
+      title: "Updates 🌠🌠🌠",
+      description: (
+        <>
           <p>Just chill for now...</p>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
@@ -57,26 +67,50 @@ export default function ChangeLog() {
         </>
       ),
     },
+    {
+      date: "15 July 2024",
+      title: "🚀 Initial Launch v0.0.1",
+      description: (
+        <>
+          <ul className="text-gray-60 list-disc space-y-1 ps-6 text-sm">
+            <li>VSCode Fork - feel right at home</li>
+            <li>Chat with AI models who have full code context</li>
+            <li>tag your files @filename</li>
+          </ul>
+          <p>Just chill for now...</p>
+        </>
+      ),
+    },
   ];
 
   return (
     <>
-      <div className="mx-8 border-red-600 px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0">
+      <div className="mx-2 border-red-600 px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0">
         <div className="border-red-600 lg:flex lg:justify-between lg:gap-4">
-          <header className="overflow-y-hidden border-2 border-solid border-red-600 lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-2/6 lg:flex-col lg:justify-between lg:py-24">
-            LeftSection
+          <header className="pt-8 overflow-y-hidden border-2 border-none border-red-600 lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-2/6 lg:flex-col lg:justify-between lg:py-32">
+            {/* LeftSection */}
+            <img src="/images/pearai-color.png" className="mt-32 mx-auto h-60 w-60" />
+            {/* <button
+              type="button"
+              className="dark:text-white mx-8 mt-1 items-center rounded-lg border border-transparent bg-gray-100 p-1 text-xs text-white-300 hover:bg-gray-100 focus:outline-none disabled:pointer-events-none disabled:opacity-50 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+            >
+              <span className="bg-white flex h-4 w-4 shrink-0 items-center justify-center rounded-full border border-gray-200 text-[10px] font-semibold uppercase text-gray-600 dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-400">
+                A
+              </span>
+              Try Pear AI - Download
+            </button> */}
           </header>
 
           {/* <!-- Right Section (scrollable) --> */}
-          <div className="min-h-screen border-2 border-none border-indigo-600 pt-24 lg:w-4/6 lg:py-24">
+          <div className="min-h-screen border-2 border-none border-indigo-600 pt-24 lg:w-4/6 lg:py-32">
             <main>
-              Right Section
-              {/* <!-- Timeline --> */}
-              {updates.map((update, index) => (
-                <TimelineItem key={index} {...update} />
-              ))}
               <div>
-                <TimelineItem
+                {/* Right Section */}
+                {/* <!-- Timeline --> */}
+                {updates.map((update, index) => (
+                  <TimelineItem key={index} {...update} />
+                ))}
+                {/* <TimelineItem
                   date="31 September 2024"
                   title="v.0.0.2 - Claude Sonnet Model, UI/UX improvements."
                   description="v.0.0.2 - Claude Sonnet Model, UI/UX improvements."
@@ -112,7 +146,7 @@ export default function ChangeLog() {
                       eius? Rem dolorem assumenda perspiciatis blanditiis
                       suscipit cumque! Commodi, consequatur itaque suscipit
                       omnis nesciunt debitis deleniti sunt at?"
-                />
+                /> */}
               </div>
               {/* <!-- End Timeline --> */}
             </main>
--- app/changelog/timeline.tsx
+++ app/changelog/timeline.tsx
@@ -10,49 +10,56 @@ export default function TimelineItem({
   description,
 }: TimelineItemProps) {
   return (
-    <div className="flex gap-x-3">
-      {/* <!-- Left Content --> */}
-      <div className="w-28 flex-shrink-0 text-end">
+    <>
+      <div className="mb-2 ml-2 mt-6 w-28 md:hidden">
         <span className="text-xs text-gray-500 dark:text-neutral-400">
           {date}
         </span>
       </div>
-      {/* <!-- End Left Content --> */}
-
-      {/* <!-- Icon --> */}
-      <div className="relative after:absolute after:bottom-0 after:start-3.5 after:top-7 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 last:after:hidden dark:after:bg-neutral-700">
-        <div className="relative z-10 flex h-7 w-7 items-center justify-center">
-          <div className="h-2 w-2 rounded-full bg-gray-400 dark:bg-neutral-600"></div>
+      <div className="flex gap-x-3">
+        {/* <!-- Left Content --> */}
+        <div className="hidden w-28 flex-shrink-0 text-end md:block">
+          <span className="text-xs text-gray-500 dark:text-neutral-400">
+            {date}
+          </span>
         </div>
-      </div>
-      {/* <!-- End Icon --> */}
+        {/* <!-- End Left Content --> */}
 
-      {/* <!-- Right Content --> */}
-      <div className="grow pb-8 pt-0.5">
-        <h3 className="dark:text-white flex gap-x-1.5 font-semibold text-gray-800">
-          {title}
-        </h3>
-        <div className="mt-1 text-sm text-gray-600 dark:text-neutral-400">
-          {description}
+        {/* <!-- Icon --> */}
+        <div className="relative after:absolute after:bottom-0 after:start-3.5 after:top-7 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 last:after:hidden dark:after:bg-neutral-700">
+          <div className="relative z-10 flex h-7 w-7 items-center justify-center">
+            <div className="h-2 w-2 rounded-full bg-gray-400 dark:bg-neutral-600"></div>
+          </div>
         </div>
-        {/* <button
+        {/* <!-- End Icon --> */}
+
+        {/* <!-- Right Content --> */}
+        <div className="grow pb-8 pt-0.5">
+          <h3 className="dark:text-white flex gap-x-1.5 font-semibold text-gray-800">
+            {title}
+          </h3>
+          <div className="mt-1 text-sm text-gray-600 dark:text-neutral-400">
+            {description}
+          </div>
+          {/* <button
           type="button"
           className="-ms-1 mt-1 inline-flex items-center gap-x-2 rounded-lg border border-transparent p-1 text-xs text-gray-500 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none disabled:pointer-events-none disabled:opacity-50 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
         >
           <img className='shrink-0 h-4 w-4 rounded-full' src='https://images.unsplash.com/photo-1659482633369-9fe69af50bfb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8auto=format&fit=facearea&facepad=3&w=320&h=320&q=80' alt='Avatar'/>
           James Collins
         </button> */}
-        <button
-          type="button"
-          className="-ms-1 mt-1 inline-flex items-center gap-x-2 rounded-lg border border-transparent p-1 text-xs text-gray-500 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none disabled:pointer-events-none disabled:opacity-50 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-        >
-          <span className="bg-white flex h-4 w-4 shrink-0 items-center justify-center rounded-full border border-gray-200 text-[10px] font-semibold uppercase text-gray-600 dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-400">
-            A
-          </span>
-          latest release
-        </button>
+          {/* <button
+            type="button"
+            className="-ms-1 mt-1 inline-flex items-center gap-x-2 rounded-lg border border-transparent p-1 text-xs text-gray-500 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none disabled:pointer-events-none disabled:opacity-50 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+          >
+            <span className="bg-white flex h-4 w-4 shrink-0 items-center justify-center rounded-full border border-gray-200 text-[10px] font-semibold uppercase text-gray-600 dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-400">
+              A
+            </span>
+            latest release
+          </button> */}
+        </div>
+        {/* <!-- End Right Content --> */}
       </div>
-      {/* <!-- End Right Content --> */}
-    </div>
+    </>
   );
 }
