ae0e4a0f82cf65d6cb07a7a668a5dbfbec72da7a
SHADOW-LIGHTS <108618163+SHADOW-LIGHTS@users.noreply.github.com>
2024-08-06 18:28:16 +0530

    new styling Geist

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./css/style.css";
 import "./globals.css";
-import { Inter, Architects_Daughter } from "next/font/google";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 import { PHProvider } from "./providers";
 import Header from "@/components/ui/header";
 import { Toaster } from "@/components/ui/sonner";
@@ -8,19 +9,6 @@ import dynamic from "next/dynamic";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { ThemeProvider } from "next-themes";
-
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-  display: "swap",
-});
-
-const architects_daughter = Architects_Daughter({
-  subsets: ["latin"],
-  variable: "--font-architects-daughter",
-  weight: "400",
-  display: "swap",
-});
 
 const PostHogPageView = dynamic(() => import("./PostHogPageView"), {
   ssr: false,
@@ -32,10 +20,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <PHProvider>
         <body
-          className={`${inter.variable} ${architects_daughter.variable} font-inter bg-background tracking-tight text-gray-900 antialiased`}
+          className={`--font-geist-sans bg-background tracking-tight text-gray-900 antialiased`}
         >
           <ThemeProvider
             attribute="class"
