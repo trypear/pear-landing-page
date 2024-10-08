ee23da987039941c0775f61e918339ca847169a2
Duke Pan <matthewdukepan@gmail.com>
2024-10-08 11:08:25 -0700

    Windows version updated

--- components/pricing.tsx
+++ components/pricing.tsx
@@ -283,15 +283,14 @@ const PricingTier: React.FC<ExtendedPricingTierProps> = ({
                         year: "numeric",
                       })}
                     </div>
-                    * Our Windows, Linux and Mac Intel versions will be back by
-                    end of week!
+                    * Our Linux and Mac Intel versions will be back by end of
+                    week!
                   </div>
                 )}
                 <div className="flex w-full max-w-md gap-2">
                   <Button
                     className={cn("rainbow-gradient", "font-bold", "flex-1")}
                     onClick={() => handleDownload("windows")}
-                    disabled
                   >
                     <WindowsLogo className="h-[18px] w-[18px] fill-white-main" />
                     Windows
