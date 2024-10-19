b19120e7109914aed4d3d4f726e7e608e30f5751
itzamanjain <aman.044063@tmu.ac.in>
2024-10-19 18:40:09 +0530

    approved blog api

---
+++ app/api/approve-blog/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createClient } from "@/utils/supabase/server";
+
+// Todo only admin can approve blog
+async function updateBlogStatus(request: NextRequest) {
+  const supabase = createClient();
+
+  try {
+    const { id } = await request.json();
+
+    const { data, error } = await supabase
+      .from("blog")
+      .update({ status: "approved" })
+      .eq("id", id);
+
+    if (error) {
+      console.error("Error updating blog status:", error);
+      return NextResponse.json(
+        { error: "Failed to update blog status" },
+        { status: 500 },
+      );
+    }
+
+    return NextResponse.json({
+      message: "Blog status updated successfully",
+      data,
+    });
+  } catch (error) {
+    console.error("Error parsing request body:", error);
+    return NextResponse.json(
+      { error: "Invalid request body" },
+      { status: 400 },
+    );
+  }
+}
+
+export const POST = updateBlogStatus;
