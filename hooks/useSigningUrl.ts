ac8bf8716970c0cdd0b73a1695d59a69f59de8b9
Abubaker779 <abubakaransari7798@gmail.com>
2024-11-19 20:22:35 +0530

    issue-#375/modified the useSigninUrl.ts to reduce the encoding

--- hooks/useSigningUrl.ts
+++ hooks/useSigningUrl.ts
@@ -1,37 +1,34 @@
 "use Client";
 
 import { useState, useEffect } from "react";
-import { useSearchParams } from "next/navigation";  
+import { useSearchParams } from "next/navigation";
 
 type UrlHookReturn = [string];
-// for handling signin URL 
-export const useSignInUrl = () : UrlHookReturn => {
-    const searchParams = useSearchParams();
-    const [callbackForDesktopApp, setCallbackForDesktopApp]= useState<string>('');
+// for handling signin URL
+export const useSignInUrl = (): UrlHookReturn => {
+  const searchParams = useSearchParams();
+  const [callbackForDesktopApp, setCallbackForDesktopApp] =
+    useState<string>("");
 
-    useEffect(()=>{
-        const callback:string = searchParams.get('callback') ?? '';
-        setCallbackForDesktopApp(callback);
-    },[searchParams]);
+  useEffect(() => {
+    const callback: string = searchParams.get("callback") ?? "";
+    setCallbackForDesktopApp(callback);
+  }, [searchParams]);
 
-    const signinUrl:string = `/signin${callbackForDesktopApp? `?callback=${encodeURIComponent(callbackForDesktopApp)}`:''}`
-    return [signinUrl]; 
-}
+  const signinUrl: string = `/signin${callbackForDesktopApp ? `?callback=${callbackForDesktopApp}` : ""}`;
+  return [signinUrl];
+};
 
+// for handling signup URL
+export const useSignUpUrl = (): UrlHookReturn => {
+  const searchParams = useSearchParams();
+  const [callbackForDesktopApp, setCallbackForDesktopApp] =
+    useState<string>("");
 
-// for handling signup URL 
-export const useSignUpUrl = () : UrlHookReturn => {
-    const searchParams = useSearchParams();
-    const [callbackForDesktopApp, setCallbackForDesktopApp] = useState<string>('');
-
-    useEffect(()=>{
-        const callback:string = searchParams.get('callback') ?? '';
-        setCallbackForDesktopApp(callback);
-    },[searchParams]);
-    const signupUrl:string = `/signup${callbackForDesktopApp? `?callback=${encodeURIComponent(callbackForDesktopApp)}`:''}`
-    return [signupUrl]
-}
-
-
-
-
+  useEffect(() => {
+    const callback: string = searchParams.get("callback") ?? "";
+    setCallbackForDesktopApp(callback);
+  }, [searchParams]);
+  const signupUrl: string = `/signup${callbackForDesktopApp ? `?callback=${callbackForDesktopApp}` : ""}`;
+  return [signupUrl];
+};
