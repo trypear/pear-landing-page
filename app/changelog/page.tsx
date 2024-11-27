f19f6a79770e96772fc10216ebb23a9f444b4a0d
Duke Pan <matthewdukepan@gmail.com>
2024-11-27 00:33:43 -0800

    subtext for changelog

--- app/changelog/page.tsx
+++ app/changelog/page.tsx
@@ -13,7 +13,10 @@ const ChangeLog: React.FC = () => {
   return (
     <div className="mx-auto mb-32 mt-36 min-h-screen px-10">
       <h1 className="mb-10 text-5xl font-bold text-primary-700">Change Logs</h1>
-      <h2>Only major version updates are shown here. For most recent updates, see the #releases channel in our Discord or our GitHub commits.</h2>
+      <h2>
+        Only major version updates are shown here. For most recent updates, see
+        the #releases channel in our Discord or our GitHub commits.
+      </h2>
       <main>
         <section>
           {/* <!-- Timeline --> */}
