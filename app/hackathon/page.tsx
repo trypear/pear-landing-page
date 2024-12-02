4f543f199bf9f259dca7758a43f2fffab09dd6b5
Duke Pan <matthewdukepan@gmail.com>
2024-12-01 17:46:02 -0800

    hackathon page

--- app/hackathon/page.tsx
+++ app/hackathon/page.tsx
@@ -3,21 +3,21 @@ import { Button } from "@/components/ui/button";
 
 export default function HackathonPage() {
   return (
-    <div className="h-screen w-screen flex justify-center overflow-hidden relative bg-gray-900 light">
+    <div className="light relative flex h-screen w-screen justify-center overflow-hidden bg-gray-900">
       <img
         src="/images/pearai-hackathon-mobile.svg"
         alt="PearAI Hackathon"
-        className="w-full h-full absolute object-cover opacity-90 md:hidden" // Show on mobile, hide on larger screens
+        className="absolute h-full w-full object-cover opacity-90 md:hidden" // Show on mobile, hide on larger screens
       />
       <img
         src="/images/pearai-hackathon.svg"
         alt="PearAI Hackathon"
-        className="w-full h-full absolute object-cover opacity-90 hidden md:block" // Hide on mobile, show on larger screens
+        className="absolute hidden h-full w-full object-cover opacity-90 md:block" // Hide on mobile, show on larger screens
       />
       <Button
         asChild
         variant="white"
-        className="relative top-[26rem] md:top-[22rem] z-10 transform hover:scale-105 transition-transform duration-200 text-xl md:text-4xl px-8 py-8"
+        className="relative top-[26rem] z-10 transform px-8 py-8 text-xl transition-transform duration-200 hover:scale-105 md:top-[22rem] md:text-4xl"
       >
         <Link href="https://forms.gle/Jph47doeUHy1CP1J6" target="_blank">
           Sign up early today
