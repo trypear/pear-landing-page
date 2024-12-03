0ed63197b422a7ed909d49d2a715b01f6e5a04fa
Duke Pan <matthewdukepan@gmail.com>
2024-12-02 17:23:03 -0800

    PearAI hackahton

--- app/hackathon/page.tsx
+++ app/hackathon/page.tsx
@@ -15,13 +15,19 @@ export default function HackathonPage() {
         className="absolute hidden h-full w-full object-cover opacity-90 md:block" // Hide on mobile, show on larger screens
       />
       <div className="flex flex-col items-center">
-        <p className="text-white-50 h1 relative mt-32 text-center w-full font-bold hidden md:block">Introducing the PearAI Hackathon</p>
-        <p className="text-white-50 h1 relative mt-32 text-center w-full font-bold text-6xl md:hidden">PearAI Hackathon</p>
-        <p className="text-gray-500 text-xl font-semibold md:text-2xl relative mt-6">Coming soon. Over $10k in cash prizes.</p>
+        <p className="h1 relative mt-32 hidden w-full text-center font-bold text-white-50 md:block">
+          Introducing the PearAI Hackathon
+        </p>
+        <p className="h1 relative mt-32 w-full text-center text-6xl font-bold text-white-50 md:hidden">
+          PearAI Hackathon
+        </p>
+        <p className="relative mt-6 text-xl font-semibold text-gray-500 md:text-2xl">
+          Coming soon. Over $10k in cash prizes.
+        </p>
         <Button
           asChild
           variant="white"
-          className="relative z-10 transform px-6 py-6 text-lg transition-transform duration-200 hover:scale-105 mt-10 md:mt-10 md:text-xl"
+          className="relative z-10 mt-10 transform px-6 py-6 text-lg transition-transform duration-200 hover:scale-105 md:mt-10 md:text-xl"
         >
           <Link href="https://forms.gle/Jph47doeUHy1CP1J6" target="_blank">
             Sign up early today
