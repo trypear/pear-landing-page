828d1e48aadd29a6bc9561648c1e0c35b2a06321
Duke Pan <matthewdukepan@gmail.com>
2024-12-02 17:22:56 -0800

    PearAI hackahton

--- app/hackathon/page.tsx
+++ app/hackathon/page.tsx
@@ -5,24 +5,29 @@ export default function HackathonPage() {
   return (
     <div className="light relative flex h-screen w-screen justify-center overflow-hidden bg-black pt-12">
       <img
-        src="/images/pearai-hackathon-mobile.svg"
+        src="/images/pearai-hackathon-mobile.png"
         alt="PearAI Hackathon"
         className="absolute h-full w-full object-cover opacity-90 md:hidden" // Show on mobile, hide on larger screens
       />
       <img
-        src="/images/pearai-hackathon.svg"
+        src="/images/pearai-hackathon.png"
         alt="PearAI Hackathon"
         className="absolute hidden h-full w-full object-cover opacity-90 md:block" // Hide on mobile, show on larger screens
       />
-      <Button
-        asChild
-        variant="white"
-        className="relative top-[26rem] z-10 transform px-8 py-8 text-xl transition-transform duration-200 hover:scale-105 md:top-[22rem] md:text-4xl"
-      >
-        <Link href="https://forms.gle/Jph47doeUHy1CP1J6" target="_blank">
-          Sign up early today
-        </Link>
-      </Button>
+      <div className="flex flex-col items-center">
+        <p className="text-white-50 h1 relative mt-32 text-center w-full font-bold hidden md:block">Introducing the PearAI Hackathon</p>
+        <p className="text-white-50 h1 relative mt-32 text-center w-full font-bold text-6xl md:hidden">PearAI Hackathon</p>
+        <p className="text-gray-500 text-xl font-semibold md:text-2xl relative mt-6">Coming soon. Over $10k in cash prizes.</p>
+        <Button
+          asChild
+          variant="white"
+          className="relative z-10 transform px-6 py-6 text-lg transition-transform duration-200 hover:scale-105 mt-10 md:mt-10 md:text-xl"
+        >
+          <Link href="https://forms.gle/Jph47doeUHy1CP1J6" target="_blank">
+            Sign up early today
+          </Link>
+        </Button>
+      </div>
     </div>
   );
 }
