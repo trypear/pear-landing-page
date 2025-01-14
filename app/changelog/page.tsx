830910c98a8055614ff09987b9023f41de0563cc
Himanshu <him.speedo@gmail.com>
2025-01-14 22:34:45 +0530

    changelog 1.5.5 - 1.6.1 (#396)
    * changelog 1.5.5 - 1.6.1
    
    * update version to 1.6.1
    
    * lint fixes

--- app/changelog/page.tsx
+++ app/changelog/page.tsx
@@ -13,10 +13,31 @@ const ChangeLog: React.FC = () => {
   return (
     <div className="mx-auto mb-32 mt-36 min-h-screen px-10">
       <h1 className="mb-10 text-5xl font-bold text-primary-700">Change Logs</h1>
-      <h2>
-        Only major version updates are shown here. For most recent updates, see
-        the #releases channel in our Discord or our GitHub commits.
-      </h2>
+      <span className="inline-block rounded-full bg-primary-200 px-4 py-2 text-sm font-medium text-primary-900">
+        Only major version updates are shown here. For most recent updates,
+        please see
+        <a
+          href="https://discord.com/invite/7QMraJUsQt"
+          className="text-black-100 font-bold hover:underline"
+        >
+          #releases
+        </a>{" "}
+        channel in{" "}
+        <a
+          href="https://discord.com/invite/7QMraJUsQt"
+          className="text-black-100 font-bold hover:underline"
+        >
+          Discord
+        </a>{" "}
+        or our{" "}
+        <a
+          href="https://github.com/trypear"
+          className="text-black-100 font-bold hover:underline"
+        >
+          github commits
+        </a>{" "}
+        in various repos.
+      </span>
       <main>
         <section>
           {/* <!-- Timeline --> */}
