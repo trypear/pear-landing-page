0d0ac2aa6a2160679b70f09d7a54a84e0828495b
Himanshu <him.speedo@gmail.com>
2025-01-15 00:08:56 +0530

    remove right arrow

--- tailwind.config.js
+++ tailwind.config.js
@@ -185,17 +185,12 @@ module.exports = {
           "0%": { opacity: "0" },
           "100%": { opacity: "1" },
         },
-        "bounce-horizontal": {
-          "0%, 100%": { transform: "translateX(0)" },
-          "50%": { transform: "translateX(3px)" },
-        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         "fadein-opacity": "fadein-opacity 0.6s ease-in-out",
         spin: "spin .5s linear infinite",
-        "bounce-horizontal": "bounce-horizontal 0.8s ease-in-out infinite",
       },
     },
   },
