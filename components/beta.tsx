440caecafc47d130086e67b15e4b440c5b455218
nang-dev <nathanang2000@gmail.com>
2025-02-28 00:58:09 -0500

    Added beta

--- components/beta.tsx
+++ components/beta.tsx
@@ -24,16 +24,19 @@ const BetaPage: React.FC<{ user: any }> = ({ user }) => {
     : {};
 
   return (
-    <section className="relative pt-8 sm:pt-12 md:pt-16 lg:pt-24" aria-label="beta downloads">
+    <section
+      className="relative pt-8 sm:pt-12 md:pt-16 lg:pt-24"
+      aria-label="beta downloads"
+    >
       <div className="absolute top-0 z-[-1] mt-[-35px] h-[140px] w-full bg-primary-800/30 blur-3xl"></div>
       <div className="mx-auto max-w-7xl px-8 sm:px-6 lg:px-20">
         <div className="flex flex-col items-center space-y-6 sm:space-y-8 md:space-y-6 lg:space-y-6">
           <header className="mx-auto mt-16 max-w-4xl space-y-4 text-center sm:mt-0 sm:space-y-6">
             <h1 className="mt-8 text-4xl font-medium leading-tight sm:text-4xl md:text-4xl lg:text-4xl">
-              Download PearAI Beta
+              Download PearAI Beta Version
             </h1>
             <p className="text-lg text-gray-600">
-              Try out the latest beta features and help us improve PearAI
+              Try out the latest beta version of PearAI and let us know what you think in <a href="https://discord.gg/AKy5FmqCkF" className="text-blue-500 hover:text-blue-600 underline">Discord</a>!
             </p>
           </header>
 
@@ -43,7 +46,10 @@ const BetaPage: React.FC<{ user: any }> = ({ user }) => {
                 <Button
                   className={cn("rainbow-gradient", "font-bold", "w-full")}
                   style={gradientStyle}
-                  onClick={() => window.location.href = "https://drive.google.com/drive/folders/1kQjtQkg8fScj-pFvVdUT1X9Bky41Kkl_?usp=sharing"}
+                  onClick={() =>
+                    (window.location.href =
+                      "https://drive.google.com/drive/folders/1kQjtQkg8fScj-pFvVdUT1X9Bky41Kkl_?usp=sharing")
+                  }
                 >
                   <AppleLogo className="mr-2 h-[18px] w-[18px] fill-current" />
                   MacOS M Chip
@@ -56,7 +62,10 @@ const BetaPage: React.FC<{ user: any }> = ({ user }) => {
               <div className="flex flex-col items-center space-y-2">
                 <Button
                   className={cn("rainbow-gradient", "font-bold", "w-full")}
-                  onClick={() => window.location.href = "https://drive.google.com/drive/folders/1n74VC3FWArMf6Td9sf0wEVzb1jMBpim_?usp=drive_link"}
+                  onClick={() =>
+                    (window.location.href =
+                      "https://drive.google.com/drive/folders/1n74VC3FWArMf6Td9sf0wEVzb1jMBpim_?usp=drive_link")
+                  }
                 >
                   <WindowsLogo className="h-[18px] w-[18px] fill-white-main" />
                   Windows
