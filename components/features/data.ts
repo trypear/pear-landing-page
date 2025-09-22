e11e1ee7d3e8757a985587c52e1c7752634c97ec
nang-dev <nathanang2000@gmail.com>
2025-09-22 11:58:11 -0400

    Added local mp4

--- components/features/data.ts
+++ components/features/data.ts
@@ -1,12 +1,10 @@
-import { BUNNY_CDN_HOST } from "@/utils/constants";
-
 export const videoData = [
   {
     id: 1,
     title: "Code automatically with PearAI Agent.",
     description:
       "PearAI Agent (powered by Roo Code / Cline*) can code and fix bugs for you!",
-    videoUrl: `${BUNNY_CDN_HOST}/pearai-agent-welcome-2.mp4`,
+    videoUrl: "/mp4/pearai-agent-welcome.mp4",
     duration: 13500,
   },
   {
@@ -14,7 +12,7 @@ export const videoData = [
     title: "Make specific in-line changes and ask questions with PearAI Chat",
     description:
       "Ask questions or generate code with the context of your @codebase for more accurate results with PearAI Chat - an AI chat assistant, powered by Continue.",
-    videoUrl: `${BUNNY_CDN_HOST}/pearai-chat-welcome-2.mp4`,
+    videoUrl: "/mp4/pearai-chat-welcome.mp4",
     duration: 10500,
   },
 ];
